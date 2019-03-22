87f402fdb056ee7a54ef4bc91efa1891bd0a261f
2stacks <2stacks@2stacks.net>
2019-03-22 14:53:50 -0400

    Added .travis.yml for integration testing

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -ev
 docker-compose up -d
-# TODO - run django server
-docker pull 2stacks/radtest
 # Wait for Postgresql to bootstrap
 sleep 15
 docker-compose ps
-docker run -it --rm --network docker-freeradius_backend 2stacks/radtest radtest testing password freeradius 2 testing123
+docker-compose logs postgres
+./django-freeradius/tests/manage.py migrate
+./django-freeradius/tests/manage.py createsuperuser --username testing --email testing@localhost
+password
+password
+./django-freeradius/tests/manage.py runserver 0.0.0.0:8000
+docker pull 2stacks/radtest
+docker-compose ps
+docker run -it --rm --network freeradius-django_backend 2stacks/radtest radtest testing password freeradius 2 testing123
