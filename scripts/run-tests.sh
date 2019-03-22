8fe41bdc35406c74ad2187a8235e354b177889e7
2stacks <2stacks@2stacks.net>
2019-03-22 15:54:32 -0400

    Added users.csv to bootstrap users database

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,12 +4,9 @@ docker-compose up -d
 # Wait for Postgresql to bootstrap
 sleep 15
 docker-compose ps
-docker-compose logs postgres
 ./django-freeradius/tests/manage.py migrate
-./django-freeradius/tests/manage.py createsuperuser --username testing --email testing@localhost
-password
-password
-./django-freeradius/tests/manage.py runserver 0.0.0.0:8000
+./django-freeradius/tests/manage.py batch_add_users --name users --file ./scripts/users.csv
+./django-freeradius/tests/manage.py runserver 0.0.0.0:8000 &
 docker pull 2stacks/radtest
 docker-compose ps
 docker run -it --rm --network freeradius-django_backend 2stacks/radtest radtest testing password freeradius 2 testing123
