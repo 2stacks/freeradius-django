78d883979c3dccffe1367cde8985cb1e60608bf3
2stacks <2stacks@2stacks.net>
2019-03-27 17:53:21 -0400

    Removed second docker-compos ps command from run-tests.sh

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,6 @@ set -ev
 docker-compose up -d
 # Wait for Postgresql to bootstrap
 sleep 15
-docker-compose ps
 ./django-freeradius/tests/manage.py migrate
 ./django-freeradius/tests/manage.py batch_add_users --name users --file ./scripts/users.csv
 ./django-freeradius/tests/manage.py runserver 0.0.0.0:8000 &
